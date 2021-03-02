d2cb3e65be9d2faef98411276f20cecb1e87abfe
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2021-03-01 23:13:34 -0500

    appendFileSync

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+// fs.writeFileSync('notes.txt', 'My name is Victor')
+
+fs.appendFileSync('notes.txt', ' I am 28 years old')
