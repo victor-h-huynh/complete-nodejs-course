9c65f4d35f62afd8050b405307560634760df842
Victor Huynh <45438361+victor-h-huynh@users.noreply.github.com>
2021-03-08 16:18:10 -0500

    argument-parsing-Yargs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
 
-const msg = getNotes();
+const command = process.argv[2]
 
-console.log(msg);
+console.log(process.argv)
 
-const greenMsg = chalk.blue.inverse.bold('Success!');
-console.log(greenMsg);
+if (command === 'add') {
+    console.log('Adding note!');
+} else if (command === 'remove') {
+    console.log('Removing note!');
+}
